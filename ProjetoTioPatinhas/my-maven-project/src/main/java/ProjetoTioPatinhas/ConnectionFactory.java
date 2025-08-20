babcf2cf41baa8157755fd8d22d7c58d51fa2a9f
Vitor <vitorapolinariosgobi@gmail.com>
2025-08-20 15:42:27 -0300

    adicionei a conexão com o servidor da fiap

--- ProjetoTioPatinhas/my-maven-project/src/main/java/ProjetoTioPatinhas/ConnectionFactory.java
+++ ProjetoTioPatinhas/my-maven-project/src/main/java/ProjetoTioPatinhas/ConnectionFactory.java
@@ -11,9 +11,9 @@ public class ConnectionFactory {
     //   jdbc:oracle:thin:@//localhost:1521/XEPDB1
     // - Se usar SID (ex.: XE antigo):
     //   jdbc:oracle:thin:@localhost:1521:XE
-    private static final String URL  = "jdbc:oracle:thin:@//localhost:1521/XEPDB1";
-    private static final String USER = "pbl_cripto";
-    private static final String PASS = "senhacripto";
+    private static final String URL  = "jdbc:oracle:thin:@oracle.fiap.com.br:1521:ORCL";
+    private static final String USER = "RM555140";
+    private static final String PASS = "Fiap#2025";
 
     public static Connection getConnection() throws SQLException {
         // Opcional com drivers modernos: DriverManager.registerDriver(new oracle.jdbc.OracleDriver());
